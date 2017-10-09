f2ba0fe4c8ad9c36bd1846866b39a1e3d7d779b4
Finezja <karolinaxpaprzycka@gmail.com>
2017-10-09 20:59:37 +0200

    zadanie 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,44 @@
 'use strict';
+
+ document.addEventListener('DOMContentLoaded', function() {
+     
+     //zadanie 1
+      
+    var materialy = document.querySelector('.for-dropdown');
+    var strony = document.querySelector('.dropdown');
+    
+    materialy.addEventListener('mouseover', function() {
+    strony.style.display = "block";
+    });
+     
+    materialy.addEventListener('mouseout', function() {
+    strony.style.display = "none";
+    });
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ }); // to musi być! odwołuje się do samego początku!
